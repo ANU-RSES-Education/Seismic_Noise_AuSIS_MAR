a343bb9d1d09e5c6ca58247d550fe6eb79592746
Louis Moresi <louis.moresi@anu.edu.au>
2020-06-23 14:39:09 +1000

    Update seismosocialdistancing_settings.py
    Dates

--- notebooks/seismosocialdistancing_settings.py
+++ notebooks/seismosocialdistancing_settings.py
@@ -12,7 +12,7 @@ start = UTCDateTime("2019-12-01")
 
 # Leaving UTCDateTime() empty means "now":
 # and this means 24 hours ago: UTCDateTime() - 24*3600
-end = UTCDateTime("2020-06-15")
+end = UTCDateTime() - 24*3600
 
 
 # This is the time it takes to be sure the data that we download is a complete record that
